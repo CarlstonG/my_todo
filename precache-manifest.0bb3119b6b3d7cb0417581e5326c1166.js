1f470f3dc272b2d63d584582606246187e7eba28
CarlstonG <carlstongrefalde@gmail.com>
2020-04-12 09:56:09 +0800

    Updates

---
+++ precache-manifest.0bb3119b6b3d7cb0417581e5326c1166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e38b3515734cab633744114a918771",
+    "url": "/my_todo/index.html"
+  },
+  {
+    "revision": "681788ebb2d4f7fb4d79",
+    "url": "/my_todo/static/css/main.c2c63651.chunk.css"
+  },
+  {
+    "revision": "4a5b92609e61c5aa280b",
+    "url": "/my_todo/static/js/2.6de047bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my_todo/static/js/2.6de047bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681788ebb2d4f7fb4d79",
+    "url": "/my_todo/static/js/main.de6421a3.chunk.js"
+  },
+  {
+    "revision": "1118cd706460184c7235",
+    "url": "/my_todo/static/js/runtime-main.ab971b0d.js"
+  }
+]);
